62131f08ef2f945d56fa32254d3f3614873b4ed3
Justin DuJardin <justin@dujardinconsulting.com>
2023-11-29 12:09:56 -0800

    chore: allow passing python to setup

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 set -e
 
+# Default Python path
+PYTHON_PATH="python3"
+
+# Check if a custom Python path is provided as the first argument
+if [ -n "$1" ]; then
+  PYTHON_PATH="$1"
+fi
+
+echo "Using Python at: $PYTHON_PATH"
+$PYTHON_PATH --version
+
 # Make the virtualenv only if the folder doesn't exist
 DIR=.env
 if [ ! -d "${DIR}" ]; then
   pip install virtualenv --upgrade
-  python3 -m virtualenv .env -p python3 || virtualenv .env -p python3
+  $PYTHON_PATH -m virtualenv .env -p $PYTHON_PATH || virtualenv .env -p $PYTHON_PATH
 fi
 
 . .env/bin/activate
