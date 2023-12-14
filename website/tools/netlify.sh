177e69443a1847740327aa9934f146df520f25d3
Justin DuJardin <justin@dujardinconsulting.com>
2023-12-14 13:19:11 -0800

    chore: really fix setup script for netlify

--- website/tools/netlify.sh
+++ website/tools/netlify.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 set -e
-sh tools/setup.sh
-echo "Building Netlify site..."
-../../.env/bin/mkdocs build
+
+# Default Python path
+PYTHON_PATH="python3"
+
+# Check if a custom Python path is provided as the first argument
+if [ -n "$1" ]; then
+  PYTHON_PATH="$1"
+fi
+
+echo "Using Python at: $PYTHON_PATH"
+$PYTHON_PATH --version
+
+# Make the virtualenv only if the folder doesn't exist
+DIR=.env
+if [ ! -d "${DIR}" ]; then
+  pip install virtualenv --upgrade
+  $PYTHON_PATH -m virtualenv .env -p $PYTHON_PATH || virtualenv .env -p $PYTHON_PATH
+fi
+
+echo "Installing/updating requirements..."
+.env/bin/mkdocs build
