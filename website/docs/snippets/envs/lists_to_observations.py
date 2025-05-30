d7ed906cc5ff2dd32db343f706e5f23d77f031c5
Justin DuJardin <justin@dujardinconsulting.com>
2025-05-30 14:16:59 -0700

    docs: fix website test snippet for new observation outputs

--- website/docs/snippets/envs/lists_to_observations.py
+++ website/docs/snippets/envs/lists_to_observations.py
@@ -1,11 +1,19 @@
-from mathy_envs import MathyEnv, MathyEnvState, MathyObservation, envs
+from mathy_envs import MathyEnv, MathyEnvState, envs
+from mathy_envs.state import MathyGraphObservation, ObservationType
 
 env: MathyEnv = envs.PolySimplify()
 state: MathyEnvState = env.get_initial_state()[0]
-observation: MathyObservation = env.state_to_observation(state)
+observation = env.state_to_observation(
+    state, obs_type=ObservationType.GRAPH, max_seq_len=100
+)
+# Narrow down to the specific observation type
+assert isinstance(observation, MathyGraphObservation)
 
-# As many nodes as values
-assert len(observation.nodes) == len(observation.values)
-# Mask is a binary validity mask of size (num_rules, num_nodes)
-assert len(observation.mask) == len(env.rules)
-assert len(observation.mask[0]) == len(observation.nodes)
+# Check actual number of nodes
+assert observation.num_nodes > 0
+assert observation.num_nodes <= 100  # should be within max_seq_len
+
+# Check the observation structure
+assert observation.adjacency is not None, "Adjacency matrix should not be None"
+assert observation.node_features is not None, "Node features should not be None"
+assert observation.action_mask is not None, "Action mask should not be None"
